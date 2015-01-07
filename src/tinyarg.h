d58efe12d8c0dcfcc7a2495682dbea864a8112fa
Stuart Wallace <stuart@wapbot.co.uk>
2015-01-07 14:31:00 +0000

    Add externs for use in C++ code

--- src/tinyarg.h
+++ src/tinyarg.h
@@ -1,25 +1,34 @@
 #ifndef __TINYARG_H__
 #define __TINYARG_H__
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 struct  tiny_args_t;
 
 bool tiny_args_parse(struct tiny_args_t* args, int argc, const char* argv[]);
 
 void tiny_args_add_bool(struct tiny_args_t** args,
-												char short_opt,
-												const char* long_opt,
-												bool* flag,
-												const char* desc);
+                        char short_opt,
+                        const char* long_opt,
+                        bool* flag,
+                        const char* desc);
 
 void tiny_args_add_str(struct tiny_args_t** args,
-											 char short_opt,
-											 const char* long_opt,
-										 	 char* str,
-											 size_t str_len,
-											 const char* desc);
+                       char short_opt,
+                       const char* long_opt,
+                       char* str,
+                       size_t str_len,
+                       const char* desc);
 
 void tiny_args_usage(const char* process_name, struct tiny_args_t* args);
 
 void tiny_args_destroy(struct tiny_args_t* args);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __TINYARG_H__ */
